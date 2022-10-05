b698e3fad7649e9eda2e668a7e9812d03ae111ea
Herman <smurovnemez@mail.ru>
2022-10-05 05:49:00 +0300

    feat: calculatingTime&JS

---
+++ JS/algorithms/calculatingTime.js
@@ -0,0 +1,12 @@
+// What do we do if we have multiple expressions/loops/etc?
+// 1 + 1 + 1 (this 3 loops)
+// n * n * n
+
+/* 
+    O(1) Running a statement
+    O(1) Value look-up on an array, obj, variable
+    O(logn) Loop that cuts problem in half every iteration 
+    O(n) Looping through the values of an array
+    O(n^2) DOUBLE NESTED LOOPS
+    O(n^3) Triple nested loops
+    */
